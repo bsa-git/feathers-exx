1f6ef96a578e24ac98e30b7eaf6e77b1ee15e573
bsa-git <bsa2657@yandex.ru>
2018-07-20 10:56:37 +0300

    Commit project

--- server/start.js
+++ server/start.js
@@ -9,19 +9,6 @@ const http = require('http');
 require('./plugins/unhandled-rejection');
 
 /**
- * Get port from environment and store in Express.
- */
-// const _port = process.env.PORT || 3000;
-// const port = normalizePort(_port);
-// app.set('port', port);
-
-/**
- * Get host from environment and store in Express.
- */
-// const _host = process.env.BASE_URL || '0.0.0.0';
-// app.set('host', _host);
-
-/**
  * Create HTTP server.
  */
 const server = http.createServer(app);
@@ -30,64 +17,45 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(app.get('port'));
-server.on('error', onError);
+// server.on('error', onError);
 server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
 
 /**
  * Event listener for HTTP server "error" event.
  */
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    const bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
+// function onError(error) {
+//     if (error.syscall !== 'listen') {
+//         throw error;
+//     }
+//
+//     const bind = typeof port === 'string'
+//         ? 'Pipe ' + port
+//         : 'Port ' + port;
+//
+//     // handle specific listen errors with friendly messages
+//     switch (error.code) {
+//         case 'EACCES':
+//             console.error(bind + ' requires elevated privileges');
+//             process.exit(1);
+//             break;
+//         case 'EADDRINUSE':
+//             console.error(bind + ' is already in use');
+//             process.exit(1);
+//             break;
+//         default:
+//             throw error;
+//     }
+// }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    const address = server.address();
+    // const addr = server.address();
+    // const bind = typeof addr === 'string'
+    //     ? 'pipe ' + addr
+    //     : 'port ' + addr.port;
+    // const address = server.address();
     debug('App is running at %s:%d in %s mode', app.get('host'), app.get('port'), app.get('env'));
     debug('Press CTRL-C to stop');
 }
