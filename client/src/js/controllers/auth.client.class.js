9771f79f19feb970179bc2c13ba44a9bb3ef67e5
bsa-git <bsa2657@yandex.ru>
2018-08-27 09:24:44 +0300

    Commit project

--- client/src/js/controllers/auth.client.class.js
+++ client/src/js/controllers/auth.client.class.js
@@ -93,6 +93,7 @@ class Auth extends Base {
                         self.userId = null;
                         const cookie = new Cookie('feathers-jwt');
                         cookie.remove('/', this.req.hostname);
+                        debug('authOAuth2.cookie.remove:', this.req.hostname);
                         const template = require('../tmpls/auth/oauth2/load.html.twig');
                         const html = template({isAuth: self.isAuth(self.app)});
                         document.getElementById('app').innerHTML = html;
